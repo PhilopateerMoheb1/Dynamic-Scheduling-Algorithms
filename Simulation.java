2177380e56c913adfef7b02f2d84baf02159e0ac
Mina Mounir <104218090+Mina-Mounir-Farid@users.noreply.github.com>
2023-04-12 13:49:54 +0200

    Create Simulation.java

---
+++ Simulation.java
@@ -0,0 +1,18 @@
+//author : Mina Mounir
+import java.awt.Graphics2D;
+/*interface Simulation
+ * description : to be implemented by Instant simulation and real time simulation classes
+*/
+public interface Simulation {
+	// returns the proper simulation object given the Queue and the flag
+	public static Simulation getInstance(OpQueue queue , boolean instantFlag )
+	{    
+		return instantFlag ? new InstantSimulation(queue) : new RealTimeSiumlation(queue);
+	}
+	//draws the simulation based inside Graphics2D object and returns it
+	public abstract Graphics2D render() ;
+	//getter for the OpQueue used for the drawing
+	public abstract OpQueue getQueue() ;
+	//steps only 1 time unit which is not supported in Instant Simulation
+	public abstract void step();
+}
