d7fa0aef9262afbae264c39ba1473da6223a3f3e
Heba2h <99084258+Heba2h@users.noreply.github.com>
2023-04-14 19:34:19 +0200

    fix: removed unused class

--- src/Comparators.java
+++ src/Comparators.java
@@ -40,18 +40,6 @@ public class Comparators {
         }
     }
 
-    static class RRComparator implements Comparator<Operation>{
-        @Override
-        public int compare(Operation o1, Operation o2) {
-            if (o1.getArrival() < o2.getArrival())
-                return -1;
-            else if (o1.getArrival() == o2.getArrival())
-                return 0;
-            else
-                return 1;
-        }
-    }
-
     static class PriorityComparator implements Comparator<Operation>{
         @Override
         public int compare(Operation o1, Operation o2) {
