836bcedf6337b6510e37c06e0f121aeb0707d849
Andrew Adel <andrewadel3322@gmail.com>
2023-04-24 19:15:10 +0200

    compare with id when the priority is equal

--- src/Comparators.java
+++ src/Comparators.java
@@ -53,11 +53,12 @@ public class Comparators {
         public int compare(Operation o1, Operation o2) {
             if (o1.getPriority() < o2.getPriority())
                 return -1;
-            else if (o1.getPriority() == o2.getPriority())
+            else if (o1.getPriority() == o2.getPriority()) {
                 if (o1.getID() < o2.getID())
                     return -1;
                 else
                     return 1;
+            }
             else
                 return 1;
         }
